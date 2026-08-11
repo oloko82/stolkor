d9e75c8ee28154dcc852c8b8e3fd21ae268090ad
Aleksander <oloko82@gmail.com>
2026-08-11 12:02:48 +0200

    Fix GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://stolkor.pl',
-  output: 'static',
+  site: 'https://oloko82.github.io',
+  base: '/stolkor',
 });
